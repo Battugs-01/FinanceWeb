780b6f6a54af5a3c672eb38d5384ecb7fe029ebf
Tugsuu <tugsking2002@gmail.com>
2022-08-19 10:25:02 +0800

    finance module ruu item add hideg bolgono

--- app.js
+++ app.js
@@ -23,28 +23,54 @@ var uiController = (function(){
 
 // Санхүүгийн хэсгийг хариуцах controller
 var financeController = (function(){
-    var Income = function(id , description , value){
-        this.id = id;
-        this.description = description;
-        this.value = value;
-    };
+   // private data
+  var Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
 
-    var Expense = function(id , description , value){
-        this.id  = id;
-        this.description = description ; 
-        this.value = value;
-    };
+  // private data
+  var Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
 
-    var data = {
-        allItems : {
-            inc : [],
-            exp : []
-        },
-        totals : {
-            inc : 0,
-            exp : 0
-        }
+  // private data
+  var data = {
+    items: {
+      inc: [],
+      exp: []
+    },
+
+    totals: {
+      inc: 0,
+      exp: 0
     }
+  };
+  return {
+    addItem: function(type, desc, val) {
+      var item, id;
+
+      if (data.items[type].length === 0) id = 1;
+      else {
+        id = data.items[type][data.items[type].length - 1].id + 1;
+      }
+
+      if (type === "inc") {
+        item = new Income(id, desc, val);
+      } else {
+        item = new Expense(id, desc, val);
+      }
+
+      data.items[type].push(item);
+    },
+
+    seeData: function() {
+      return data;
+    }
+  };  
 })();
 
 // Санхүүгийн болон дэлгэцийн controller Ийг хооронд нь зангидах controller
@@ -53,9 +79,11 @@ var appController = (function(uiCtrl , fnCtrl){
     // EventListener ажиллах үед ажиллах дараагийн үйлдлүүдийг хариуцсан функц 
     var ctrlAddItem = function(){
                 // 1рт : Оруулах өгөгдлийг дэлгэцээс олж авна
-                console.log(uiCtrl.getInput());
+                var input = uiCtrl.getInput();
+                console.log(input);
                 // 2рт : Олж авсан өгөгдлүүдээ санхүүгийн controllert дамжуулж тэнд хаднална
-        
+                fnCtrl.addItem(input.type , input.description , input.value);
+            
                 // 3рт : Олж авсан өгөгдлүүдээ веб дээрээ тохирох хэсэгт нь гаргана
         
                 // 4рт : Төсвийг тооцоолно 
